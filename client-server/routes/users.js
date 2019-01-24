eca92a5a0b8a45f46f52e068c25402b850aaf4db
paulowebphp <paulowebphp@gmail.com>
2019-01-24 19:53:37 -0200

    Criando e configurando as rotas para os métodos GET, POST, PUT e DELETE com recursos do Restify

--- client-server/routes/users.js
+++ client-server/routes/users.js
@@ -21,18 +21,110 @@ var client = restify.createJsonClient(
 
 
 
-/* GET users listing. */
+
+
 router.get('/', function(req, res, next)
 {
+
   client.get('/users', function(err, request, response, obj)
   {
     
     assert.ifError(err);
   
-    res.end(JSON.stringify(obj, null, 2));
+    res.json(obj);
 
   });
 
-});//END router.get
+});//END route
 
+
+
+
+
+
+
+router.get('/:id', function(req, res, next)
+{
+
+  client.get(`/users/${req.params.id}`, function(err, request, response, obj)
+  {
+    
+    assert.ifError(err);
+  
+    res.json(obj);
+
+  });
+
+});//END route
+
+
+
+
+
+
+router.put('/:id', function(req, res, next)
+{
+
+  client.put(`/users/${req.params.id}`, req.body, function(err, request, response, obj)
+  {
+    
+    assert.ifError(err);
+  
+    res.json(obj);
+
+  });
+
+});//END route
+
+
+
+
+
+
+
+
+router.delete('/:id', function(req, res, next)
+{
+
+  /** Restify chama o método DELETE apenas como 'del' */
+  client.del(`/users/${req.params.id}`, function(err, request, response, obj)
+  {
+    
+    assert.ifError(err);
+  
+    res.json(obj);
+
+  });
+
+});//END route
+
+
+
+
+
+
+router.post('/', function(req, res, next)
+{
+
+  client.post('/users', req.body, function(err, request, response, obj)
+  {
+    
+    assert.ifError(err);
+  
+    res.json(obj);
+
+  });
+
+});//END route
+
+
+
+
+
+
+
+
+
+
+/** MODULE EXPORTS */
 module.exports = router;
