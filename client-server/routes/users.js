8a820e4e94ede32aca5f9abffd88f0a739b7e70c
paulowebphp <paulowebphp@gmail.com>
2019-01-24 16:48:27 -0200

    Instalando o Restify pelo NPM para começar a configurar as Rotas

--- client-server/routes/users.js
+++ client-server/routes/users.js
@@ -1,9 +1,38 @@
+/** EXPRESS */
 var express = require('express');
+
+/** ASSERT */
+var assert = require('assert');
+
+/** RESTIFY */
+var restify = require('restify-clients');
+
 var router = express.Router();
 
+
+
+/** Creates a JSON client */
+var client = restify.createJsonClient(
+{
+  
+  url: 'http://localhost:4000'
+
+});//end restify.createJsonClient
+
+
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', function(req, res, next)
+{
+  client.get('/users', function(err, request, response, obj)
+  {
+    
+    assert.ifError(err);
+  
+    res.end(JSON.stringify(obj, null, 2));
+
+  });
+
+});//END router.get
 
 module.exports = router;
